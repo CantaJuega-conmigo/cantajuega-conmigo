5fa6095aa22329bb237340652a93f2794d4104a0
ArceDaniel <jonathandanielarce9@gmail.com>
2023-03-29 19:01:40 -0300

    hice algunas cosas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+
+const contentPaths = [
+  "./app//*.{js,ts,jsx,tsx}",
+  "./pages//.{js,ts,jsx,tsx}",
+  "./components/**/.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: contentPaths,
   theme: {
     extend: {
-      colors: { 
+      colors: {
         black: "#1b1b1b",
         withe: "#f5f5f5",
         orange: "#ffc172",
-        blue: "#26798e", 
-        green: "#63caa7", 
+        blue: "#26798e",
+        green: "#63caa7",
         cream: "#ffe3b3",
-      }
+      },
+      fontFamily: {
+        main: ["./public/fonts/ITCKRIST/ITCKRIST.ttf"],
+        sans: ["Roboto", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
