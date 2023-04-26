35fc8c434b9e4819503cd9f7e75200765294ea24
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-26 19:05:42 -0300

    change one chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   content: contentPaths,
   theme: {
     extend: {
-      screens: {
+      screen: {
         "2xl": { max: "1535px" },
         // => @media (max-width: 1535px) { ... }
 
