1b243610ab3fb72da07fa1335dbc127eb37c4b2c
unknown <joakig6@gmail.com>
2023-04-29 03:32:34 -0300

    mensajes visibles de errores de las validaciones de registro y login , alerta de campos vacios

--- src/components/global/Register.tsx
+++ src/components/global/Register.tsx
@@ -2,6 +2,8 @@ import { AUTH_MODAL_TYPE } from '@/utils';
 import styles from '../../styles/register.module.css'
 import { useState } from 'react'
 import { registerError } from './FormsErrors';
+import { IoMdClose } from 'react-icons/io';
+import { FcGoogle } from 'react-icons/fc';
 
 interface RegisterProps {
    handleOpen: (name: AUTH_MODAL_TYPE) => void;
@@ -32,6 +34,7 @@ interface ErrorProps {
   });
 
   const [error, setError] = useState<ErrorProps>({});
+  const [visibleErrors,setVisibleErrors]=useState<boolean>(false)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -51,7 +54,13 @@ interface ErrorProps {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(Object.keys(error).length) return console.log(error);
+    if(Object.keys(error).length){
+       error.global&&alert(error.global)
+       return setVisibleErrors(true),
+       setTimeout(() => {
+        setVisibleErrors(false)
+       }, 10000);
+      }
      console.log('Creado')
   };
 
@@ -67,25 +76,36 @@ interface ErrorProps {
    return (
     <div className={styles.Container}>
           
-    <section className={styles.secondContainer}>
-     <article>
-          <button className={styles.Close}
-            onClick={()=> handleOpen(AUTH_MODAL_TYPE.REGISTER)} 
-          >
-            X
-          </button>
+    <section className={`${styles.secondContainer} bg-white`}>
+     <article className='flex justify-end'>
+     <button
+          className={'m-1'}
+          onClick={() => handleOpen(AUTH_MODAL_TYPE.REGISTER)}
+        > 
+          <IoMdClose
+            className="text-3xl cursor-pointer hover:text-blue hover:scale-110
+             ease-in-out transition-all"
+          />
+        </button>
         </article>
-     <h1>CREA UN USUARIO:</h1>
+     <h1 className='m-5 mb-7 text-2xl'>CREA UN USUARIO:</h1>
          <form className={styles.inputsContainer} onSubmit={handleSubmit}>
               
              <label htmlFor="">NOMBRE</label>
              <input type="text" onChange={handleChange} name='firstName' /> 
+             {visibleErrors&&error.firstName&&<span className='text-red-500'>{error.firstName}</span>}
              <label htmlFor="">APELLIDO</label>
              <input type="text" onChange={handleChange} name='lastName' /> 
+             {visibleErrors&&error.lastName&&<span className='text-red-500'>{error.lastName}</span>}
+
              <label htmlFor="">CORREO ELECTRÓNICO</label>
              <input type="text" onChange={handleChange} name='email'/> 
+             {visibleErrors&&error.email&&<span className='text-red-500'>{error.email}</span>}
+
              <label htmlFor="">CONTRASEÑA</label> 
              <input type="password" onChange={handleChange} name='password' id='password' />
+             {visibleErrors&&error.password&&<span className='text-red-500'>{error.password}</span>}
+
                <section><label  >mostrar contraseña</label> <input type="checkbox" onChange={showPassword} /></section> 
             <section className={styles.paswwordSection}>
              <input type="checkbox" name="" className={styles.checkBox} id="Passwordvisible" />
@@ -102,8 +122,15 @@ interface ErrorProps {
                  <h5>O</h5>
                  <section></section>
                </div>
-               <button type='button' className={styles.googleButton} >Login with google</button>
-  
+               <button
+            type="button"
+            className={`${styles.googleButton} flex items-center font-bold justify-center mb-32
+          py-3 w-3/4`}
+        
+          >
+            <FcGoogle className="text-3xl mr-2" />
+            Login with google
+          </button>
          </form>
    
         
