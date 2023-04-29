0af2c3598c116af22b2b2ab5d56817509cbf1e1b
ArceDaniel <jonathandanielarce9@gmail.com>
2023-04-29 19:40:31 -0300

    add membership type and create query file

---
+++ src/functions/memership.query.ts
@@ -0,0 +1,18 @@
+import { Membership } from "@/types/membership.type";
+import axios from "./axios";
+import React from "react";
+
+export async function getAllMebreships(
+  state: React.Dispatch<React.SetStateAction<Membership[]>>
+): Promise<void> {
+  try {
+    const query = await axios.get("/membership");
+    if ("error" in query.data) throw new Error(query.data.error);
+    const data: Membership[] = query.data;
+    state(data);
+    return;
+  } catch (error) {
+    // Handle error here
+    console.log(error);
+  }
+}
