8326cc15f66659eac6973acdcfeff34c49cf2cec
unknown <joakig6@gmail.com>
2023-06-13 00:42:23 -0300

    responsive metodologies

--- src/components/home/metodologia/Metodologias.tsx
+++ src/components/home/metodologia/Metodologias.tsx
@@ -25,26 +25,33 @@ export default function Metodologias(){
     setModal(true)
   }
    return (
-    <div className={`${styles.Container} h-[20rem] `} id='metodologia'>
-      <section className='flex flex-col items-center justify-center h-full  relative'>
+    <div className={`${styles.Container} h-[25rem] `} id='metodologia'>
+      <section className='flex flex-col items-center justify-center h-full   relative'>
         <article className='z-20 h-1/6 '>
         <h1 className=' text-5xl text-yellow'>Metodología</h1>
         </article>
-        <article className='z-20 flex h-5/6 flex-col items-center justify-around'>   
-          <button className='h-[3rem] w-[17rem] flex items-center justify-between' onClick={setContentModal} value={Neuroeducacion.title}>
+
+        <article className='z-20 flex h-5/6 flex-col items-center justify-around  w-full'>   
+          <button className='h-[3rem]  w-[25%] max-w-[25rem] flex items-center justify-between' onClick={setContentModal} value={Neuroeducacion.title}>
             <span className='bg-blue w-[3.2rem] h-full flex items-center justify-center rounded-lg text-3xl'>1</span>
-            <span className=' w-[12rem] h-full items-center flex text-blue text-2xl '>Neuroeducación</span>
+            <span className=' w-[80%]  h-full items-center flex text-blue justify-center
+            text-base  2xl:text-5xl xl:text-4xl lg:text-3xl'>
+              Neuroeducación</span>
           </button>
-          <button className='h-[3rem] w-[17rem] flex items-center justify-between' onClick={setContentModal} value={Music_art.title}>
+          <button className='h-[3rem]  w-[25%] max-w-[25rem] flex items-center justify-between' onClick={setContentModal} value={Music_art.title}>
             <span className='bg-blue w-[3.2rem] h-full flex items-center justify-center rounded-lg text-3xl'>2</span>
-            <span className=' w-[12rem] h-full items-center flex text-blue text-2xl '>Música y arte</span>
+            <span className=' w-[80%]  h-full items-center flex text-blue justify-center
+            text-base  2xl:text-5xl xl:text-4xl lg:text-3xl'>
+              Música y arte</span>
           </button>
-          <button className='h-[3rem] w-[17rem] flex items-center justify-between' onClick={setContentModal} value={VideoModelling.title}>
+          <button className='h-[3rem]  w-[25%] max-w-[25rem] flex items-center justify-between' onClick={setContentModal} value={VideoModelling.title}>
             <span className='bg-blue w-[3.2rem] h-full flex items-center justify-center rounded-lg text-3xl'>3</span>
-            <span className=' w-[12rem] h-full items-center flex text-blue text-2xl '>Video modeling</span>
+            <span className=' w-[80%]  h-full items-center flex text-blue justify-center
+            text-base  2xl:text-5xl xl:text-4xl lg:text-3xl'>
+              Video modeling</span>
           </button>
         </article>
-      <Image alt='cjimgs' src={image} className=' z-10 absolute top-0 h-full w-auto left-[10rem]'/>
+      <Image alt='cjimgs' src={image} className=' z-10 absolute top-0 min-w-[15rem] w-[39%] h-auto  lg:h-full lg:w-auto left-[0%] 2xl:left-[10%]'/>
       </section>
       {modal&&<Modal closeModal={closemodal} text={actualModal.text} title={actualModal.title}/>}
     </div>
